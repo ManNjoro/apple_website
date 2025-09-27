9ec2b950d8609027b240f4c7e51eeba2e9961940
ManNjoro <elijohnmwoho@gmail.com>
2025-09-27 23:23:54 +0300

    Refactored App component: replaced h1 element with main element and updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ const App = () => {
   
 
   return (
-    <h1 class="text-3xl font-bold bg-blue underline">
-    Hello world!
-  </h1>
+    <main className="bg-black">
+      
+    </main>
   )
 }
 
