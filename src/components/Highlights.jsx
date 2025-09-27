9b2f3f033a3d9225e781321e6ec3545fdcda33a7
ManNjoro <elijohnmwoho@gmail.com>
2025-09-28 00:50:05 +0300

    Added VideoCarousel component and updated JSX structure in Highlights component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React from "react";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
@@ -14,7 +15,7 @@ const Highlights = () => {
       opacity: 1,
       y: 0,
       duration: 1,
-      stagger: 0.25
+      stagger: 0.25,
     });
   }, []);
   return (
@@ -28,16 +29,18 @@ const Highlights = () => {
             Get the highlights
           </h1>
           <div className="flex flex-wrap items-end gap-5">
-            <p className="link">Watch the film
-
-            <img src={watchImg} alt="watch" className="!ml-2" />
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="!ml-2" />
             </p>
-            <p className="link">Watch the event
-
-            <img src={rightImg} alt="watch" className="!ml-2" />
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="watch" className="!ml-2" />
             </p>
           </div>
         </div>
+
+        <VideoCarousel />
       </div>
     </section>
   );
