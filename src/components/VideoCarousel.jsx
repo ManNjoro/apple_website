ba84dcd02276a95aa7b61da289571484c206ff03
ManNjoro <elijohnmwoho@gmail.com>
2025-10-01 00:38:33 +0300

    Refactor VideoCarousel component: update GSAP animations, add event listeners, and modify CSS classes

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -24,20 +24,25 @@ const VideoCarousel = () => {
   const { isEnd, isLastVideo, isPlaying, startPlay, videoId } = video;
 
   useGSAP(() => {
-    gsap.to('#video', {
+    gsap.to("#slider", {
+      transform: `translateX(${-100 * videoId}%)`,
+      duration: 2,
+      ease: "power2.inOut",
+    });
+    gsap.to("#video", {
       scrollTrigger: {
-        trigger: '#video',
-        toggleActions: 'restart none none none'
+        trigger: "#video",
+        toggleActions: "restart none none none",
       },
       onComplete: () => {
         setVideo((prev) => ({
           ...prev,
           startPlay: true,
-          isPlaying: true
-        }))
-      }
-    })
-  }, [isEnd, videoId])
+          isPlaying: true,
+        }));
+      },
+    });
+  }, [isEnd, videoId]);
 
   useEffect(() => {
     if (loadedData.length > 3) {
@@ -49,18 +54,63 @@ const VideoCarousel = () => {
     }
   }, [startPlay, videoId, isPlaying, loadedData]);
 
-  const handleLoadedMetadata = (i, e) => setLoadedData((prev) => [...prev, e])
-  console.log("loadedData", loadedData)
+  const handleLoadedMetadata = (i, e) => setLoadedData((prev) => [...prev, e]);
+  console.log("loadedData", loadedData);
   useEffect(() => {
-    const currentProgress = 0;
+    let currentProgress = 0;
     let span = videoSpanRef.current;
 
     if (span[videoId]) {
       // animate the progress of the video
       let anim = gsap.to(span[videoId], {
-        onUpdate: () => {},
-        onComplete: () => {},
+        onUpdate: () => {
+          const progress = Math.ceil(anim.progress() * 100);
+          if (progress != currentProgress) {
+            currentProgress = progress;
+
+            gsap.to(videoDivRef.current[videoId], {
+              width:
+                window.innerWidth < 760
+                  ? "10vw"
+                  : window.innerWidth < 1200
+                  ? "10vw"
+                  : "4vw",
+            });
+
+            gsap.to(span[videoId], {
+              width: `${currentProgress}%`,
+              backgroundColor: "white",
+            });
+          }
+        },
+        onComplete: () => {
+          if (isPlaying) {
+            gsap.to(videoDivRef.current[videoId], {
+              width: "12px",
+            });
+            gsap.to(span[videoId], {
+              backgroundColor: "#afafaf",
+            });
+          }
+        },
       });
+
+      if (videoId === 0) {
+        anim.restart();
+      }
+
+      const animUpdate = () => {
+        anim.progress(
+          videoRef.current[videoId].currentTime /
+            hightlightsSlides[videoId].videoDuration
+        );
+      };
+
+      if (isPlaying) {
+        gsap.ticker.add(animUpdate);
+      } else {
+        gsap.ticker.remove(animUpdate);
+      }
     }
   }, [videoId, startPlay]);
 
@@ -76,6 +126,9 @@ const VideoCarousel = () => {
         setVideo((prev) => ({ ...prev, isLastVideo: false, videoId: 0 }));
         break;
       case "play":
+        setVideo((prev) => ({ ...prev, isPlaying: !prev.isPlaying }));
+        break;
+      case "pause":
         setVideo((prev) => ({ ...prev, isPlaying: !prev.isPlaying }));
         break;
       default:
@@ -94,7 +147,15 @@ const VideoCarousel = () => {
                   playsInline={true}
                   preload="auto"
                   muted
+                  className={`${
+                    list.id === 2 && "translate-x-44"
+                  } pointer-events-none`}
                   ref={(el) => (videoRef.current[i] = el)}
+                  onEnded={() =>
+                    i !== 3
+                      ? handleProcess("video-end", i)
+                      : handleProcess("video-last")
+                  }
                   onPlay={() => {
                     setVideo((prevVideo) => ({
                       ...prevVideo,
