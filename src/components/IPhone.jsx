db36daa24ee8b014073ef3152d071bee1a80739a
ManNjoro <elijohnmwoho@gmail.com>
2025-10-04 13:14:32 +0300

    Refactored IPhone.jsx and ModelView.jsx files: removed unused import, updated class name in ModelView.jsx

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -6,8 +6,8 @@ Source: https://sketchfab.com/3d-models/apple-iphone-15-pro-max-black-df17520841
 Title: Apple iPhone 15 Pro Max Black
 */
 
-import React, { useEffect, useRef } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
+import { useEffect } from 'react'
 import * as THREE from 'three'
 
 export default function Model(props) {
