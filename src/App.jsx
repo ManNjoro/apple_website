efb98dd64048e292365674f6a0a22c84bbcc1bad
ManNjoro <elijohnmwoho@gmail.com>
2025-10-04 13:37:06 +0300

    Added Sentry integration to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
 import Model from "./components/Model"
-
+import * as Sentry from '@sentry/react';
 
 const App = () => {
   
 
   return (
     <main className="bg-black">
-      <Navbar />
+      {/* <Navbar />
       <Hero />
       <Highlights />
-      <Model />
+      <Model /> */}
+      <button
+      className="cursor-pointer"
+      onClick={() => {
+        throw new Error('This is your first error!');
+      }}
+    >
+      Break the world
+    </button>
     </main>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7f1ea765947a5d3c83dbee90f37486c7@o4509719875616768.ingest.de.sentry.io/4510130712870992",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
